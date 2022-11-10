82e00e20a49d04643f2c14e439773d8940452b7c
Rushikesh Suryawanshi <109567768+Rushigithub22@users.noreply.github.com>
2022-11-10 23:58:34 +0530

    Update Day01_Sum of Numbers.java

--- Day01_Sum of Numbers/Day01_Sum of Numbers.java
+++ Day01_Sum of Numbers/Day01_Sum of Numbers.java
@@ -1,7 +1,7 @@
 /*
 📌TOPIC: SUM OF TWO NUMBERS IN JAVA
 📌QUESTION: Add two numbers where variable a=10, b=5 in simple java program?
-
+There are two ways to find the sum of two numbers in Java.
 To find the sum or addition of two numbers using the method and command-line arguments the sum of three numbers, and the sum of n numbers.
 In Java, finding the sum of two or more numbers is very easy. 
 First, declare and initialize two variables to be added. Another variable to store the sum of numbers. 
@@ -21,6 +21,36 @@ public class Main {
     }
     
 }
+
+//📍Another example
+import java.util.Scanner;  
+public class SumOfNumbers2  
+{  
+public static void main(String args[])  
+{  
+int x, y, sum;  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the first number: ");  
+x = sc.nextInt();  
+System.out.print("Enter the second number: ");  
+y = sc.nextInt();  
+sum = sum(x, y);  
+System.out.println("The sum of two numbers x and y is: " + sum);  
+}  
+//method that calculates the sum  
+public static int sum(int a, int b)  
+{  
+int sum = a + b;  
+return sum;  
+}  
+}  
+
+/*OUTPUT
+Enter the first number: 34
+Enter the second number: 12
+The sum of two numbers x and y is: 46
+*/
+
 
 /*
 📌DESCRIPTION:
