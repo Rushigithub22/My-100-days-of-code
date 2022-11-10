2cad83630fa48c959802d4ae4d599f0d08a5d1b3
Rushikesh Suryawanshi <109567768+Rushigithub22@users.noreply.github.com>
2022-11-10 18:42:33 +0530

    My-100-days-of-code

---
+++ power.java
@@ -0,0 +1,14 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class power {
+    public static void main(String[] args) {
+        {
+            int x;
+            x = 3;
+            // System.out.println(x);
+        }
+        int x = 69;
+        System.out.println(x);
+
+    }
+}
