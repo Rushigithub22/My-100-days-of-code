27ce22f9eddfb986220e99a4932b9ae8f52bdabf
Rushikesh Suryawanshi <109567768+Rushigithub22@users.noreply.github.com>
2022-11-10 23:55:39 +0530

    Update Day05_If-else Conditional Statement.java

--- Day05_If-else Conditional Statement/Day05_If-else Conditional Statement.java
+++ Day05_If-else Conditional Statement/Day05_If-else Conditional Statement.java
@@ -1,8 +1,13 @@
-//Given task about Given an integer,n , perform the following conditional actions: If  is odd, print Weird If  is even and in the inclusive range of  to , print Not Weird
-//If  is even and in the inclusive range of  to , print Weird
-//If  is even and greater than , print Not Weird
-//Complete the stub code provided in your editor to print whether or not  is weird.
-//sample input (0)=3 //sample output= Weird //sample input (1)= 24 //sample output= Not Weird.
+/* 
+📌TOPIC: If-else Conditional Statement
+📌QUESTION: if else conditional statement
+
+Given task about Given an integer,n , perform the following conditional actions: If  is odd, print Weird If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Complete the stub code provided in your editor to print whether or not  is weird.
+sample input (0)=3 //sample output= Weird //sample input (1)= 24 //sample output= Not Weird.
+*/
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -30,3 +35,22 @@ public class Day5 {
         scanner.close();
     }
 }
+
+/*
+📌DESCRIPTION:
+Java supports the usual logical conditions from mathematics:
+
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+Equal to a == b
+Not Equal to: a != b
+You can use these conditions to perform different actions for different decisions.
+
+Java has the following conditional statements:
+
+Use if to specify a block of code to be executed, if a specified condition is true
+Use else to specify a block of code to be executed, if the same condition is false
+Use else if to specify a new condition to test, if the first condition is false
+Use switch to specify many alternative blocks of code to be executed
