65e2c060218aa809770a78d0468353475e7b9296
Rushikesh Suryawanshi <109567768+Rushigithub22@users.noreply.github.com>
2022-11-11 00:33:41 +0530

    Update Day30_Tag Content Extractor.java

--- Day30_Tag Content Extractor/Day30_Tag Content Extractor.java
+++ Day30_Tag Content Extractor/Day30_Tag Content Extractor.java
@@ -70,3 +70,4 @@ public class Solution{
 The HTML code <h1>Hello World</h2> is not valid, because the text starts with an h1 tag and ends with a non-matching h2 tag.
 Tags can be nested, but content between nested tags is considered not valid. For example, in <h1><a>contents</a>invalid</h1>, contents is valid but invalid is not valid.
 Tags can consist of any printable characters.
+*/
